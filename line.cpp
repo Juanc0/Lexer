4e32d0aac89711487acf2eb72697b3bee2442bd0
Juan Pablo Ovalle Sánchez <juanpos97@gmail.com>
2018-03-10 20:07:10 -0500

    Auto stash before merge of "master" and "origin/master"

--- line.cpp
+++ line.cpp
@@ -4,16 +4,24 @@
 #include <string>
 using namespace std;
 
+void func(istream &input){
+  string line;
+  input >> line;
+  // getline(input, line);
+  cout << line;
+}
+
 int main() {
   ifstream programfile("program-example.txt");
-  string line;
-  int i = 1;
+  func(programfile);
+  // string line;
+  // int i = 1;
 
-  while(getline(programfile, line)) {
-    istringstream iss(line);
-    cout << i << "  " << line << "\n";
-    i ++;
-  }
+  // while(getline(programfile, line)) {
+  //   istringstream iss(line);
+  //   cout << i << "  " << line << "\n";
+  //   i ++;
+  // }
 
   programfile.close();
 
