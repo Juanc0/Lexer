91cb28d7de889d82c730c850c7940c2f6f10546d
Yelis3 <yeli_andre_@hotmail.com>
2018-04-01 13:03:16 -0500

    engine firsts

--- engine.cpp
+++ engine.cpp
@@ -16,14 +16,19 @@ class Grammar{
 	public:
 		Grammar(string ifname, string ofname);
 		~Grammar();
-		void print();
+		void printRules();
+		void printFirst();
 		bool isVariable(string str);
 		bool isTerminal(string str);
-		vector<string> first(string str);
-		void generateFirst();
+		void appendWithoutEpsilon(vector<string> aux, vector<string> aux2);
+		void appendWithoutDuplicates(vector<string> aux, string terminal);
+		bool hasTerminal(vector<string> rule, string terminal);
+		int getNonTerminalPosition(string str);
+		vector<string> generateFirst(string str);
+		void generateFirsts();
 };
 Grammar::Grammar(string ifname, string ofname)
-	:ifs("grammarTest.g4"), ofs("syntactic.cpp"){
+	:ifs("grammar.g4"), ofs("syntactic.cpp"){
 	string line, lineaux, lineaux2;
 	int i;
 	while(getline(ifs, line)){
@@ -56,14 +61,23 @@ Grammar::Grammar(string ifname, string ofname)
 Grammar::~Grammar(){
 	ifs.close();
 }
-void Grammar::print(){
+void Grammar::printRules(){
 	for(int i=0; i<variables.size(); i++){
 		cout << "[" << variables[i] << "]";
 		for(int k=0; k<rules[i].size(); k++){
 			cout << "{";
 			for(int j=0; j<rules[i][k].size(); j++)
-				cout << "(" << rules[i][k][j] << ")";
+				cout << "<" << rules[i][k][j] << ">";
 			cout << "}";
+		}
+		cout << endl;
+	}
+}
+void Grammar::printFirst(){
+	for(int i=0; i<variables.size(); i++){
+		cout << "[" << variables[i] << "]";
+		for(int j=0; j<first[i].size(); j++){
+			cout << "<" << first[i][j] << ">";
 		}
 		cout << endl;
 	}
@@ -76,27 +90,66 @@ bool Grammar::isTerminal(string str){
 	if(str[0] == '\'' && str[str.length()-1] == '\'')return true;
 	return false;
 }
-vector<string> Grammar::first(string str){
-	for(int i=0; i<variables.size(); i++){		
-		for(int j=0; j<rules[i].size(); j++){
-			for(int k=0; k<rules[i][j].size(); k++){
-				if(isTerminal(rules[i][j][k]))
-					cout << "yea" << endl;
-			}
-		}
+void Grammar::appendWithoutEpsilon(vector<string> aux, vector<string> aux2){
+	for(int i=0; i<aux.size(); i++){
+		if(aux2[i] != "epsilon")
+			appendWithoutDuplicates(aux, aux2[i]);
 	}
 }
-void Grammar::generateFirst(){
-	for(int i=0; i<variables.size(); i++){
-		first.push_back(first(variables[i]));
+void Grammar::appendWithoutDuplicates(vector<string> aux, string terminal){
+	for(int i=0; i<aux.size(); i++){
+		if(aux[i] == terminal)
+		return;
+	}
+	aux.push_back(terminal);
+}
+bool Grammar::hasTerminal(vector<string> rule, string terminal){
+	for (int i=0; i<rule.size(); i++)
+		if(rule[i] == terminal)return true;
+	return false;
+}
+int Grammar::getNonTerminalPosition(string str){
+	for(int i=0; i<variables.size(); i++)
+		if(variables[i] == str)return i;
+}
+vector<string> Grammar::generateFirst(string str){
+	int i = getNonTerminalPosition(str);
+	vector<string> ret;
+	for(int j=0; j<rules[i].size(); j++){
+		if(isTerminal(rules[i][j][0])){
+			appendWithoutDuplicates(ret, rules[i][j][0]);
+			continue;
+		}
+		for(int k=0; k<rules[i][j].size(); k++){
+			vector<string> aux2 = generateFirst(rules[i][j][k]);
+			// appendWithoutEpsilon(ret, aux2);
+			break;
+			// if(hasTerminal(aux2, 'epsilon')){
+			// 	if(k==rules[i][j].size()-1)
+			// 		appendWithoutDuplicates(ret, "epsilon");
+			// 	}else{
+			// 		if(isTerminal(rules[i][j][k+1])){
+			// 			appendWithoutDuplicates(ret, rules[i][j][k+1])
+			// 	}
+			// }else break;
+		}
+
+		// for(int k=0; k<rules[i][j].size(); k++){}
+	}
+	return ret;
+}
+void Grammar::generateFirsts(){
+	for(int i=0; i<=variables.size(); i++){
+		first.push_back(generateFirst(variables[i]));
 	}
 }
 
 // void generate first
 int main(){
-	Grammar* G = new Grammar("grammarTest.g4", "syntactic.cpp");
-	G->print();
-	G->generateFirst();
-	G->print();
+	Grammar* G = new Grammar("grammar.g4", "syntactic.cpp");
+	// G->printRules();
+	G->generateFirsts();
+	G->printFirst();
+	// G->printRules();
 	delete G;
 }
--- grammar.g4
+++ grammar.g4
@@ -1,9 +1,9 @@
 /* Statements */
-program					:		n statement2 n | epsilon
+program					:		n statement2 n | 'epsilon'
 
 statements			:		n '{' n statment2 n '}' enter | enter statement enter
-statements2 		:		statement statements3 | epsilon
-statements3			:		enter statements2 | epsilon
+statements2 		:		statement statements3 | 'epsilon'
+statements3			:		enter statements2 | 'epsilon'
 statement				:		element | routine
 
 element					:		element2 | assignment
@@ -29,9 +29,9 @@ for							:		'for' identifier 'in' for_param statements
 for_param				:		esp_operator | esp_operation | identifier | function_call
 while						:		'while' condition statements
 if							:		'if' condition statements else
-else						:		'else' statements | epsilon
+else						:		'else' statements | 'epsilon'
 condition				:		element2 | '(' ')'
-condition1			:		'(' condition ')' | epsilon
+condition1			:		'(' condition ')' | 'epsilon'
 
 /* log, leer and importar */
 
@@ -39,23 +39,23 @@ log							:		log '(' element2 ')'
 leer						:		leer '(' identifier ')'
 importar				: 	'importar' importar1 | 'desde' importar1 'importar' identifier
 importar1				: 	identifier importar2
-importar2				: 	'.' importar1 | epsilon
+importar2				: 	'.' importar1 | 'epsilon'
 
 /* Function and Function_call */
 
 function_call		:		identifier '(' c_parameters ')'
-c_parameters		:		c_parameters1 | epsilon
+c_parameters		:		c_parameters1 | 'epsilon'
 c_parameters1		:		element c_parameters2
-c_parameters2		:		',' c_parameters1 | epsilon
+c_parameters2		:		',' c_parameters1 | 'epsilon'
 
 function				:		'funcion' identifier '(' parameters ')' function_stm 'end' 'funcion'
-parameters			:		parameters1 | epsilon
+parameters			:		parameters1 | 'epsilon'
 parameters1			:		identifier parameters2
-parameters2			:		',' parameters1 | epsilon
+parameters2			:		',' parameters1 | 'epsilon'
 
 function_stm		:		enter statements2 function_stm1
-function_stm1		:		'retorno' return_stm enter function_stm | epsilon
-return_stm			:		element2 | epsilon
+function_stm1		:		'retorno' return_stm enter function_stm | 'epsilon'
+return_stm			:		element2 | 'epsilon'
 
 /* Terminal Symbols and Tokens */
 
@@ -63,11 +63,11 @@ negation				:		'!' element1
 assignment			:		identifier '=' element2
 array						:		'[' array2 ']'
 array2					:		element2 array3
-array3					:		',' array2 | epsilon
+array3					:		',' array2 | 'epsilon'
 
 number					:		token_integer | token_float
 string 					:		token_string
 boolean					:		'true' | 'false'
 
 enter						:		'\n' n
-n								:		enter | epsilon
+n								:		enter | 'epsilon'
